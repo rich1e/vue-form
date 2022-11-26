237ad09d2c81282d478e77a1e4f27b7d776bec95
Richie <yuqi.gong.home@gmail.com>
2022-11-26 10:50:02 +0800

    add typescript-plugin-css-modules

--- src/example/jsxForm/index.tsx
+++ src/example/jsxForm/index.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: yuqigong@outlook.com
  * @Date: 2022-11-24 09:46:15
- * @LastEditors: yuqigong@outlook.com
- * @LastEditTime: 2022-11-25 18:31:48
+ * @LastEditors: rich1e
+ * @LastEditTime: 2022-11-26 10:44:01
  * @FilePath: /vue-form/src/example/jsxForm/index.tsx
  * @Description:
  *
@@ -31,6 +31,7 @@ import {
   ElRadio,
   ElInputNumber,
 } from 'element-plus';
+
 import {
   InInput,
   InCheckbox,
@@ -40,194 +41,143 @@ import {
   CustInput,
 } from '/@/example/jsx/index';
 import { checkEmpty } from '/@/components/DynamicForm/src/hooks/useValidator';
+import style from './index.module.scss';
 
 type FormRefType = InstanceType<typeof ElForm>;
 
 export default defineComponent({
   name: 'JsxForm',
   setup(_) {
-    const renderModernForm = () => {
-      const formModel = reactive({
-        username: '',
-        secondname: '',
-        avatar: false,
-        city: '',
+    const formRef = ref<InstanceType<typeof ElForm> | null | any>(null);
+
+    const formModel = reactive({
+      username: '',
+      gender: '',
+      secondname: '',
+      married: false,
+      city: '',
+      referees: [],
+      slots: 1,
+    });
+
+    const formRules = {
+      username: [{ validator: checkEmpty, trigger: 'blur' }],
+      gender: [{ validator: checkEmpty, trigger: 'blur' }],
+      secondname: [{ validator: checkEmpty, trigger: 'blur' }],
+      married: [{ validator: checkEmpty, trigger: 'blur' }],
+      referees: [{ validator: checkEmpty, trigger: 'blur' }],
+    };
+
+    const onSubmit = () => {
+      formRef.value.validate((valid: any) => {
+        if (valid) {
+          console.log('submit!');
+          console.log(formModel);
+        } else {
+          console.log('error submit!');
+          return false;
+        }
       });
+    };
 
-      formModel.username = 'tes';
+    const onRest = () => {
+      formRef.value.resetFields();
+    };
 
-      const formJson3 = [
-        {
-          contorl: CustInput,
-          label: 'User name',
-          props: {
-            modelValue: formModel.username,
-            // onInput: ($event: any): any => {
-            //   console.log('searchText', $event.target.value);
-            //   formModel.username = $event.target.value;
-            // },
-            'onUpdate:modelValue': ($event: any) => {
-              console.log('User name', $event);
-              formModel.username = $event;
-            },
-          },
-        },
-        {
-          contorl: InInput,
-          label: 'Second name',
-          props: {
-            vModel: formModel.secondname,
-            modelValue: formModel.secondname,
-            'onUpdate:modelValue': ($event: any) => {
-              console.log('Second name', $event);
-              formModel.secondname = $event;
-            },
-          },
-        },
-      ];
-
-      const formJson = [
-        {
-          type: 'input',
-          label: 'User name',
-          value: formModel.username,
-          props: {
-            modelValue: formModel.username,
-            'onUpdate:modelValue': ($event: any) => {
-              console.log('username', $event);
-              formModel.username = $event;
-            },
-          },
-        },
-        // {
-        //   type: 'input',
-        //   label: 'Second name',
-        //   value: formModel.secondname,
-        //   props: {
-        //     modelValue: formModel.secondname,
-        //     'onUpdate:modelValue': ($event: any) => {
-        //       console.log('secondname', $event);
-        //       formModel.secondname = $event;
-        //     },
-        //   },
-        // },
-        // {
-        //   type: 'switch',
-        //   label: 'Avatar',
-        //   value: formModel.avatar,
-        //   props: {
-        //     modelValue: formModel.avatar,
-        //     'onUpdate:modelValue': ($event: any) => {
-        //       console.log('Avatar', $event);
-        //       formModel.avatar = $event;
-        //     },
-        //   },
-        // },
-        // {
-        //   type: 'select',
-        //   label: 'City name',
-        //   value: formModel.city,
-        //   props: {},
-        // },
-      ];
-
-      const onSubmit = () => {
-        console.log(formModel);
-      };
-
-      return (
-        <>
-          <ElForm>
-            {formJson.map((item) => {
-              const getComponent = (component: Component, props?: any) => {
-                console.log('getComponent', props);
-                return <component {...props} />;
-              };
-
-              const renderFields = (type: string, value: any, props?: any) => {
-                const contorlType: any = {
-                  input: InInput,
-                  switch: ElSwitch,
-                  select: ElSelect,
-                };
-
-                // console.log('renderFields', contorlType);
-                // console.log('renderFields', value);
-
-                const getContorl = (type: string, props?: any) => {
-                  return contorlType[type];
-                };
-
-                const contorls = getContorl(type);
-
-                const contorls2 = computed(() => getContorl(type));
-
-                // console.log('contorls', contorls);
-
-                // return <contorls2.value />;
-                // return <contorls {...props} />;
-                return <contorls v-model={props.modelValue} />;
-              };
-
-              return (
-                <ElFormItem label={item.label}>
-                  {/* {getComponent(item.contorl, item.props)} */}
-                  {renderFields(item.type, item.value, item.props)}
-                  {/* <InInput v-model={form.username} /> */}
-                </ElFormItem>
-              );
-            })}
-            <ElFormItem>
-              <ElButton onClick={onSubmit}>Submit</ElButton>
-              <ElButton>Cancel</ElButton>
-              <ElButton>Rest</ElButton>
-            </ElFormItem>
-          </ElForm>
-        </>
-      );
+    const onChangeNumber = (value: any) => {
+      console.log(value);
     };
 
     const renderPopularForm = () => {
-      const formRef = ref<InstanceType<typeof ElForm> | null | any>(null);
-
-      const formModel = reactive({
-        username: '',
-        gender: '',
-        secondname: '',
-        married: false,
-        city: '',
-        referees: [],
-        slots: 1,
-      });
-
-      const formRules = {
-        username: [{ validator: checkEmpty, trigger: 'blur' }],
-        gender: [{ validator: checkEmpty, trigger: 'blur' }],
-        secondname: [{ validator: checkEmpty, trigger: 'blur' }],
-        married: [{ validator: checkEmpty, trigger: 'blur' }],
-        referees: [{ validator: checkEmpty, trigger: 'blur' }],
+      const renderFieldsByTemplate = () => {
+        return (
+          <>
+            <ElFormItem label={'User name'} prop={'username'}>
+              <ElInput
+                modelValue={formModel.username}
+                onUpdate:modelValue={(value) => (formModel.username = value)}
+              />
+            </ElFormItem>
+            <ElFormItem label={'Second name'} prop={'secondname'}>
+              <ElInput
+                modelValue={formModel.secondname}
+                onUpdate:modelValue={(value) => (formModel.secondname = value)}
+              />
+            </ElFormItem>
+            <ElFormItem label={'Gender'} prop={'gender'}>
+              <ElRadioGroup
+                modelValue={formModel.gender}
+                onUpdate:modelValue={(value) => (formModel.gender = value)}
+              >
+                {[
+                  { label: 1, title: 'male' },
+                  { label: 2, title: 'female' },
+                ].map((item) => {
+                  return <ElRadio label={item.label}>{item.title}</ElRadio>;
+                })}
+              </ElRadioGroup>
+            </ElFormItem>
+            <ElFormItem label={'Married'} prop={'married'}>
+              <ElSwitch
+                v-model={formModel.married}
+                // modelValue={formModel.married}
+                // onUpdate:modelValue={(value: any) => (formModel.married = value)}
+              />
+            </ElFormItem>
+            <ElFormItem label={'City from'} prop={'city'}>
+              <ElSelect v-model={formModel.city}>
+                {[
+                  { vlaue: 0, label: 'Beijing' },
+                  { vlaue: 1, label: 'Shanghai' },
+                ].map((item) => {
+                  return (
+                    <ElOption
+                      label={item.label}
+                      value={item.vlaue}
+                      key={item.vlaue}
+                    />
+                  );
+                })}
+              </ElSelect>
+            </ElFormItem>
+            <ElFormItem label={'Referees'} prop={'referees'}>
+              <ElCheckboxGroup
+                // v-model={formModel.referees}
+                modelValue={formModel.referees}
+                onUpdate:modelValue={(value: any) =>
+                  (formModel.referees = value)
+                }
+              >
+                {['colleagues', 'friends', 'teachers'].map((item: any) => {
+                  return <ElCheckbox label={item} />;
+                })}
+              </ElCheckboxGroup>
+            </ElFormItem>
+            <ElFormItem label={'Slots Test'} prop={'slots'}>
+              <ElInputNumber
+                v-model={formModel.slots}
+                min={1}
+                max={10}
+                onChange={onChangeNumber}
+              />
+            </ElFormItem>
+          </>
+        );
       };
 
-      const onSubmit = () => {
-        formRef.value.validate((valid: any) => {
-          if (valid) {
-            console.log('submit!');
-            console.log(formModel);
-          } else {
-            console.log('error submit!');
-            return false;
-          }
-        });
-      };
+      return (
+        <ElForm ref={formRef} model={formModel} rules={formRules}>
+          {renderFieldsByTemplate()}
+          <ElFormItem>
+            <ElButton onClick={onSubmit}>Submit</ElButton>
+            <ElButton>Cancel</ElButton>
+            <ElButton onClick={onRest}>Rest</ElButton>
+          </ElFormItem>
+        </ElForm>
+      );
+    };
 
-      const onRest = () => {
-        formRef.value.resetFields();
-      };
-
-      const onChangeNumber = (value: any) => {
-        console.log(value);
-      };
-
+    const renderModernForm = () => {
       const renderFields = () => {
         const formJson = [
           {
@@ -399,85 +349,8 @@ export default defineComponent({
         });
       };
 
-      const renderFieldsByTemplate = () => {
-        return (
-          <>
-            <ElFormItem label={'User name'} prop={'username'}>
-              <ElInput
-                modelValue={formModel.username}
-                onUpdate:modelValue={(value) => (formModel.username = value)}
-              />
-            </ElFormItem>
-            <ElFormItem label={'Second name'} prop={'secondname'}>
-              <ElInput
-                modelValue={formModel.secondname}
-                onUpdate:modelValue={(value) => (formModel.secondname = value)}
-              />
-            </ElFormItem>
-            <ElFormItem label={'Gender'} prop={'gender'}>
-              <ElRadioGroup
-                modelValue={formModel.gender}
-                onUpdate:modelValue={(value) => (formModel.gender = value)}
-              >
-                {[
-                  { label: 1, title: 'male' },
-                  { label: 2, title: 'female' },
-                ].map((item) => {
-                  return <ElRadio label={item.label}>{item.title}</ElRadio>;
-                })}
-              </ElRadioGroup>
-            </ElFormItem>
-            <ElFormItem label={'Married'} prop={'married'}>
-              <ElSwitch
-                v-model={formModel.married}
-                // modelValue={formModel.married}
-                // onUpdate:modelValue={(value: any) => (formModel.married = value)}
-              />
-            </ElFormItem>
-            <ElFormItem label={'City from'} prop={'city'}>
-              <ElSelect v-model={formModel.city}>
-                {[
-                  { vlaue: 0, label: 'Beijing' },
-                  { vlaue: 1, label: 'Shanghai' },
-                ].map((item) => {
-                  return (
-                    <ElOption
-                      label={item.label}
-                      value={item.vlaue}
-                      key={item.vlaue}
-                    />
-                  );
-                })}
-              </ElSelect>
-            </ElFormItem>
-            <ElFormItem label={'Referees'} prop={'referees'}>
-              <ElCheckboxGroup
-                // v-model={formModel.referees}
-                modelValue={formModel.referees}
-                onUpdate:modelValue={(value: any) =>
-                  (formModel.referees = value)
-                }
-              >
-                {['colleagues', 'friends', 'teachers'].map((item: any) => {
-                  return <ElCheckbox label={item} />;
-                })}
-              </ElCheckboxGroup>
-            </ElFormItem>
-            <ElFormItem label={'Slots Test'} prop={'slots'}>
-              <ElInputNumber
-                v-model={formModel.slots}
-                min={1}
-                max={10}
-                onChange={onChangeNumber}
-              />
-            </ElFormItem>
-          </>
-        );
-      };
-
       return (
         <ElForm ref={formRef} model={formModel} rules={formRules}>
-          {/* {renderFieldsByTemplate()} */}
           {renderFields()}
           <ElFormItem>
             <ElButton onClick={onSubmit}>Submit</ElButton>
@@ -492,10 +365,17 @@ export default defineComponent({
       return (
         <>
           <h1>jsxForm</h1>
-          <ElDivider>Popular Form</ElDivider>
-          {renderPopularForm()}
-          <ElDivider>Modern Form</ElDivider>
-          {renderModernForm()}
+          <div class={style['form-wrapper']}>
+            <div class={style['form-wrapper_item']}>
+              <ElDivider>Popular Form</ElDivider>
+              {renderPopularForm()}
+            </div>
+
+            <div class={style['form-wrapper_item']}>
+              <ElDivider>Modern Form</ElDivider>
+              {renderModernForm()}
+            </div>
+          </div>
         </>
       );
     };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,18 @@
+/*
+ * @Author: rich1e
+ * @Date: 2022-11-11 18:43:42
+ * @LastEditors: rich1e
+ * @LastEditTime: 2022-11-26 10:30:38
+ */
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module '*.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
 }
